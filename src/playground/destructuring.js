a2e9014a701577dca1bf7d5840b170b89c093a7b
partyzant <partyzant>
2017-10-18 23:30:27 +0200

    redux

---
+++ src/playground/destructuring.js
@@ -0,0 +1,41 @@
+// object destructuring
+/*const person = {
+  name: 'Dupa',
+  age: 23423,
+  location: {
+    city: 'Chujnia',
+    temp: -32323
+  }
+};
+
+const {name: firstName = 'anonymus', age} = person;
+console.log(`${firstName} is ${age}`);
+
+const {city, temp: temperature} = person.location;
+console.log(`it's ${temperature} in ${city}`);
+
+
+const book = {
+  title: 'Chuj dupa i gałęzi kupa',
+  author: 'Pan Bóg',
+  publisher: {
+    name: 'Życie'
+  }
+};
+
+const {name: publisherName = 'chuj go wie'} = book.publisher;
+console.log(publisherName);
+*/
+
+
+// array destructuring
+const address = ['123 S dupa street', 'Wawa', 'Mazowieckie', '24-233'];
+
+const [, city, state = 'Nie wiadomo'] = address;
+console.log(`You are in ${city} ${state}`);
+
+
+const item = ['Coffee', '4', '6', '10'];
+
+const [prod, ,price] = item;
+console.log(`A medium ${prod} costs ${price}`);
