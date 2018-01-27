559520323814c769f166edae73639c2a85fd85d0
partyzant <partyzant>
2018-01-27 22:44:06 +0100

    setup prod webpack build

--- src/app.js
+++ src/app.js
@@ -12,14 +12,14 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1500 }));
+/*store.dispatch(addExpense({ description: 'Water bill', amount: 4500, createdAt: 1500 }));
 store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(state.filters);
-console.log(visibleExpenses);
+console.log(visibleExpenses);*/
 
 const jsx = (
   <Provider store={store}>
